4103b2b1817f4d2bc3feac40ad8eefac5de4ca85
Pratyush Pattnaik <59652297+Pratyushstark@users.noreply.github.com>
2023-07-10 00:17:31 +0530

    Add files via upload

---
+++ streamlit-palmapi.py
@@ -0,0 +1,48 @@
+#Install necessary dependencies
+import streamlit as st
+import pprint
+import google.generativeai as palm
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import VertexAI
+from langchain import PromptTemplate, LLMChain
+from gtts import gTTS
+from io import BytesIO
+import tempfile
+
+#Mention your api key here provided to you by https://makersuite.google.com/app
+palm.configure(api_key='AIzaSyCbz90FBE6_HjUdBPtwLNEbaG_IgJAl7G8')
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+
+#Title of application
+st.title("A chatbot with the capabilities of PalmAI 🤖")
+
+#Setting up the prompt area of app
+prompt = st.text_input("Enter your question here..")
+if prompt:
+    completion = palm.generate_text(
+        model=model,
+        prompt=prompt,
+        temperature=0,
+        # The maximum length of the response
+        max_output_tokens=800
+)
+    st.write(completion.result)
+else:
+    st.write("type something and hit enter...")
+
+#Using google-text-to-speech library
+tts = gTTS(completion.result, lang='en')
+
+# Save audio to temporary file
+with tempfile.NamedTemporaryFile(delete=False) as fp:
+    tts.save(fp.name)
+    audio_path = fp.name
+
+# Read the audio file
+with open(audio_path, 'rb') as f:
+    audio_bytes = f.read()
+
+# Play the audio
+st.audio(audio_bytes, format='audio/mp3')
